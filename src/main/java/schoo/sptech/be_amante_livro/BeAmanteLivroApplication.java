cc035a824c29bfbdecb1248ce029860129c7cfeb
pdrzewski <pedro.martins@sptech.school>
2026-03-09 09:09:12 -0300

    base back-end para utilizar gitignore

---
+++ src/main/java/schoo/sptech/be_amante_livro/BeAmanteLivroApplication.java
@@ -0,0 +1,13 @@
+package schoo.sptech.be_amante_livro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAmanteLivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeAmanteLivroApplication.class, args);
+	}
+
+}
