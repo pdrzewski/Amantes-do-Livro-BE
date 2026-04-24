d94f95ce6bfb1d72d85ed0249318db27f4abf305
httpsrenan <renan.pereira@sptech.school>
2026-04-24 15:37:26 -0300

    arrumando add em massa

--- src/main/java/schoo/sptech/be_amante_livro/repository/ExemplarRepository.java
+++ src/main/java/schoo/sptech/be_amante_livro/repository/ExemplarRepository.java
@@ -3,7 +3,11 @@ package schoo.sptech.be_amante_livro.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import schoo.sptech.be_amante_livro.model.Exemplar;
 
+import java.util.Optional;
+
 public interface ExemplarRepository extends JpaRepository<Exemplar, Integer> {
 
     boolean existsByLivroIdLivro(Integer idLivro);
+
+    Optional<Exemplar> findByLivro_IdLivroAndCondicao_IdCondicao(Integer idLivro, Integer idCondicao);
 }
