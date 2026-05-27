6f91ad050a63e9e88bb2c82a7cec5efd1cbf0d23
pdrzewski <pedro.martins@sptech.school>
2026-05-27 18:08:13 -0300

    feat: cria teste de cenario de acerto e erro de busca por ID de livros

--- src/test/java/schoo/sptech/be_amante_livro/livro/LivroServiceTests.java
+++ src/test/java/schoo/sptech/be_amante_livro/livro/LivroServiceTests.java
@@ -9,9 +9,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import schoo.sptech.be_amante_livro.dto.LivroRequestDto;
 import schoo.sptech.be_amante_livro.dto.LivroResponseDto;
-import schoo.sptech.be_amante_livro.mapper.LivroMapper;
+import schoo.sptech.be_amante_livro.exception.LivroNaoEncontradoException;
 import schoo.sptech.be_amante_livro.model.Autor;
 import schoo.sptech.be_amante_livro.model.Editora;
 import schoo.sptech.be_amante_livro.model.Livro;
@@ -26,6 +28,7 @@ import static org.mockito.ArgumentMatchers.any;
 @ExtendWith(MockitoExtension.class)
 public class LivroServiceTests {
 
+    private static final Logger log = LoggerFactory.getLogger(LivroServiceTests.class);
     @Mock
     private LivroRepository livroRepository;
 
@@ -91,6 +94,37 @@ public class LivroServiceTests {
             Mockito.when(livroRepository.save(any())).thenReturn(null);
 
             Assertions.assertThrows(NullPointerException.class, () -> livroService.cadastrar(dto));
+        }
+    }
+
+    @Nested
+    @DisplayName("Deve testar método de busca")
+    class buscar {
+
+        @Test
+        @DisplayName("Deve buscar e retornar um livro por ID com sucesso")
+        void deveBuscarERetornarLivroPorIdTest() {
+            Livro livro = new Livro();
+            livro.setIdLivro(1);
+
+            Integer id = 1;
+
+            Mockito.when(livroRepository.findById(id)).thenReturn(Optional.of(livro));
+
+            LivroResponseDto resultado = livroService.buscarPorId(id);
+
+            Assertions.assertNotNull(resultado);
+            Assertions.assertEquals(id, resultado.getIdLivro());
+        }
+
+        @Test
+        @DisplayName("Deve testar quando livro não é encontrado pelo ID")
+        void deveVerificarQuandoLivroNaoEncontradoPeloIdTest() {
+            Integer id = 12;
+
+            Mockito.when(livroRepository.findById(id)).thenReturn(Optional.empty());
+
+            Assertions.assertThrows(LivroNaoEncontradoException.class, () -> livroService.buscarPorId(id));
         }
     }
 }
