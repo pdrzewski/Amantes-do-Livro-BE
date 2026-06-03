7ee0e73561c2c93c41814aad7070e43d3d38f57d
pdrzewski <pedro.martins@sptech.school>
2026-06-03 00:10:31 -0300

    feature: adiciona classe GlobalExceptionHandler

---
+++ src/main/java/schoo/sptech/be_amante_livro/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package schoo.sptech.be_amante_livro.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleBadRequest(IllegalArgumentException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public String handleUnauthorized(UsernameNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFound(EntityNotFoundException ex) {
+        return ex.getMessage();
+    }
+}
